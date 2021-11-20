483790bf97238fe805d19cf8e435f09e5649d8fa
kento <kento@toriwatari.bird>
2021-11-20 19:56:28 +0900

    ✨ Update

--- 01/lisp.lisp
+++ 01/lisp.lisp
@@ -1,3 +1,22 @@
+#|
+(format t "Hello Lisp")
+; Hello Lisp
+
+(print "Hello Lisp")
+; "Hello Lisp"
+
+(defparameter *small* 1)
+  (print *small*)
+  ; 1
+
+(defparameter *small* 1)
+	(print *small*)
+	; 1
+
+(defparameter *small* 3)
+	(print *small*)
+	; 3
+
 (defvar *small* 1)
 	(print *small*)
 	; 1
@@ -6,4 +25,52 @@
 	(print *small*)
 	; 1
 
+(defun function_name (arg))
 
+(defun test_func (arg)
+  (print
+    (concatenate 'string arg "!!!")
+  )
+)
+
+(test_func "Hello World")
+; "Hello World!!!"
+
+(defparameter *small* 3)
+(defparameter *big* 10)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1)
+)
+
+(print
+  (guess-my-number)
+)
+; 6
+|#
+
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1)
+)
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number)
+)
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number)
+)
+
+(print (smaller))
+; 25
+
+(print (bigger))
+; 37
+
+(print (smaller))
+; 31
