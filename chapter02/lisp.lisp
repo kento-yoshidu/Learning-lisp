5c194d76c93460f2a362606d8713ae68d703a4be
kento <kento@toriwatari.bird>
2021-11-27 20:30:45 +0900

    ✨ Update

--- chapter02/lisp.lisp
+++ chapter02/lisp.lisp
@@ -1,4 +1,3 @@
-#|
 (format t "Hello Lisp")
 ; Hello Lisp
 
@@ -6,64 +5,56 @@
 ; "Hello Lisp"
 
 (defparameter *small* 1)
-  (print *small*)
-  ; 1
+(print *small*)
+; 1
 
 (defparameter *small* 1)
-	(print *small*)
-	; 1
+(print *small*)
+; 1
 
 (defparameter *small* 3)
-	(print *small*)
-	; 3
+(print *small*)
+; 3
 
 (defvar *small* 1)
-	(print *small*)
-	; 1
+(print *small*)
+; 1
 
 (defvar *small* 3)
-	(print *small*)
-	; 1
+(print *small*)
+; 1
 
 (defun function_name (arg))
 
 (defun test_func (arg)
   (print
-    (concatenate 'string arg "!!!")
-  )
-)
+    (concatenate 'string arg "!!!")))
 
-(test_func "Hello World")
+( test_func "Hello World")
 ; "Hello World!!!"
 
 (defparameter *small* 3)
 (defparameter *big* 10)
 
 (defun guess-my-number ()
-  (ash (+ *small* *big*) -1)
-)
+  (ash (+ *small* *big*) -1))
 
-(print
-  (guess-my-number)
-)
+(print (guess-my-number))
 ; 6
 
 (defparameter *small* 1)
 (defparameter *big* 100)
 
 (defun guess-my-number ()
-  (ash (+ *small* *big*) -1)
-)
+  (ash (+ *small* *big*) -1))
 
 (defun smaller ()
   (setf *big* (1- (guess-my-number)))
-  (guess-my-number)
-)
+  (guess-my-number))
 
 (defun bigger ()
   (setf *small* (1+ (guess-my-number)))
-  (guess-my-number)
-)
+  (guess-my-number))
 
 (print (smaller))
 ; 25
@@ -77,41 +68,29 @@
 (defun start_over()
   (defparameter *small* 1)
   (defparameter *big* 100)
-  (guess-my-number)
-)
+  (guess-my-number))
 
-(print
-  (start_over)
-)
+(print (start_over))
 
 (let
   ((a 1))
-  (print a)
-)
+  (print a))
 
 (print a)
 
 (let
   ((a 2)(b 10))
-  (print (* a b))
-  ; 20
-)
+  (print (* a b)))
+; 20
 
 (flet
-  ((f (n) (+ n 10)))
-)
+  ((f (n) (+ n 10))))
 
-(print
-  (f 10)
-  ; undefined function F
-)
-|#
+(print (f 10))
+; undefined function F
 
 (labels
-  (
-    (f (n) (+ n 10))
-    (g (n) (+ (f n) 6))
-  )
-  (print (g 10))
-  ; 26
-)
+  ((f (n) (+ n 10))
+    (g (n) (+ (f n) 6)))
+  (print (g 10)))
+; 26
