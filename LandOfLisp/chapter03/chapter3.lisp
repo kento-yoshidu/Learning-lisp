3fbe2075784da72a379fa5a0f5da023a5d3a63f4
kento <kento@toriwatari.bird>
2022-11-16 06:40:11 +0900

    ✨ Update

--- LandOfLisp/chapter03/chapter3.lisp
+++ LandOfLisp/chapter03/chapter3.lisp
@@ -43,3 +43,52 @@
 
 (print (cons 'I (cons 'LOVE (cons 'LISP ()))))
 ;=> (I LOVE LISP)
+
+; car cdr
+; carはセルの最初のスロット
+(print (car (cons 'I '(LOVE LISP))))
+;=> I
+
+; firstでもOK
+(print (first '(1 2 3)))
+
+; cdrは2番目以降
+(print (cdr (cons 'I '(LOVE LISP))))
+;=> (LOVE LISP)
+
+; restでもOK
+(print (rest '(1 2 3)))
+;=> (2 3)
+
+; card carとcdrの組み合わせ
+; 2番目のコンスセル(cdr)の1番目の要素(car)を返す
+
+(print (cadr (cons '(1 2 3) '(4 5 6))))
+;=> 4
+
+; caddrならcdr(4 5 6)のcdr(5 6)のcar(5)
+(print (caddr (cons '(1 2 3) '(4 5 6))))
+;=> 5
+
+; caddrならcdr(4 5 6)のcdr(5 6)のcdr(6)のcar(6)
+(print (cadddr (cons '(1 2 3) '(4 5 6))))
+;=> 6
+
+; cdarもある
+; car(1 2 3)のcdr(2 3)
+(print (cdar (cons '(1 2 3) '(4 5 6))))
+;=> (2 3)
+
+; cddarならcarのcdrのcdr
+(print (cddar (cons '(1 2 3) '(4 5 6))))
+;=> (3)
+
+; cadadrみたいなもの出来る。
+; cdr(cons('(4 5 6) '(7 8 9)))のcar((4 5 6))のcdr(5 6)のcar(5)
+(print (cadadr (cons '(1 2 3) (cons '(4 5 6) '(7 8 9)))))
+;=> 5
+
+
+
+; https://cortyuming.hateblo.jp/entry/20080717/p1
+
